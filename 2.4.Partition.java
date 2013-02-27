4100493822ee0b513b211a3e83d08924fdfe2d45
quantumrose <quantumrose@outlook.com>
2013-02-27 14:20:48 -0500

    2.4 Partition

---
+++ 2.4.Partition.java
@@ -0,0 +1,72 @@
+// Cracking the Coding Interview 2.3
+// Write code to partition a linked list around a value x, such that all nodes less than x come before all nodes greater than of equal to x.
+
+public class Partition {
+	// create two linked lists to hold node less than x and nodes greater than or equal to x, then merge
+	// O(n) time, O(1) space
+	public static Node partition(Node n, int x) {
+		Node lHead = null, lTail = null; // nodes less than x
+		Node gHead = null, gTail = null; // nodes greater than or equal to x
+
+		while (n != null) {
+			if (n.data < x) {
+				if (lHead == null) {
+					lHead = n;
+					lTail = n;
+				}
+				else {
+					lTail.next = n;
+					lTail = n;
+				}
+			}
+			else {
+				if (gHead == null) {
+					gHead = n;
+					gTail = n;
+				}
+				else {
+					gTail.next = n;
+					gTail = n;
+				}
+			}
+			n = n.next;
+		}
+
+		if (lHead == null) {
+			return gHead;
+		}
+		else {
+			lTail.next = gHead;
+			return lHead;
+		}
+	}
+}
+
+class Node {
+	Node next = null;
+	int data;
+
+	public Node(int data) {
+		this.data = data;
+	}
+
+	public void appendToTail(int data) {
+		Node n = this;
+
+		while (n.next != null) {
+			n = n.next;
+		}
+
+		n.next = new Node(data);
+	}
+
+	public void display() {
+		Node n = this;
+
+		while (n != null) {
+			System.out.print(n.data + " ");
+			n = n.next;
+		}
+		System.out.println();
+	}
+}
